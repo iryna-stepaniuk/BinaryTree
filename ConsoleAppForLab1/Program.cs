fdfed23eb4e2e7bfc630d33550483886982f6318
Iryna Stepaniuk <Iryna_Stepaniuk@epam.com>
2020-10-02 23:13:17 +0300

    add lab1 create Collection - Binary Tree

---
+++ ConsoleAppForLab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using lab1BinaryTree;
+using System;
+
+namespace ConsoleAppForLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BinaryTree<string> languages = new BinaryTree<string>("P");
+            languages.Added += (object sender, AddToBinaryTreeEventArgs<string> arg) => Console.WriteLine(arg.Message);
+            languages.CheckIfContains += (object sender,CheckIfBinaryTreeContainsItemEventArgs<string> arg) => Console.WriteLine(arg.Message);
+
+            languages.Add("A", "D", "C", "B", "F", "E", "Z", "Y", "K", "L", "M", "N", "Y", "Q");
+           
+            foreach (string language in languages.InOrder(languages.Node))
+            {
+                Console.WriteLine(language);
+            }
+            Console.WriteLine(languages.Count);
+            Console.WriteLine(languages.Contains("Java"));
+            Console.ReadKey();
+        }
+    }
+}
